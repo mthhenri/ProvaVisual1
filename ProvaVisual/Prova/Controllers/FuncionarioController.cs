5ec26e20f1798c07d710a24df777204cc9dce5ea
mthhenri <mathsilvaesilva@gmail.com>
2023-10-06 09:15:27 -0300

    Funcionario model

--- ProvaVisual/Prova/Controllers/FuncionarioController.cs
+++ ProvaVisual/Prova/Controllers/FuncionarioController.cs
@@ -1,7 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Prova.Data;
 
 namespace Prova.Controllers;
 public class FuncionarioController : ControllerBase
 {
+    private readonly AppDatabase _ctx;
+    public FuncionarioController(AppDatabase ctx)
+    {
+        _ctx = ctx;
+    }
 
+    
 }
