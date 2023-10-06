66fb11ea814df21a0a8a43c6f91d53de3fa9cdad
mthhenri <mathsilvaesilva@gmail.com>
2023-10-06 09:06:29 -0300

    Banco

---
+++ ProvaVisual/Prova/Data/AppDatabase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Prova.Data;
+public class AppDatabase : DbContext
+{
+    public AppDatabase(DbContextOptions<AppDatabase> options) : base(options) { }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=database.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { 
+        base.OnModelCreating(modelBuilder);
+    }
+}
