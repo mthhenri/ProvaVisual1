7ed925faf3d484082e55cde4fbd4c4b8afceeb49
aaaaa <aaaaa@aaaa.br>
2023-10-06 09:44:56 -0300

    Coisas do Controller

--- ProvaVisual/Prova/Controllers/FolhaPagamentoController.cs
+++ ProvaVisual/Prova/Controllers/FolhaPagamentoController.cs
@@ -1,15 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Prova.Data;
+using Microsoft.EntityFrameworkCore;
+using Prova.Controllers; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Prova.Controllers;
-public class FolhaPagamentoController : ControllerBase
+namespace Prova.Controllers
 {
-    private readonly AppDatabase _ctx;
-    public FolhaPagamentoController(AppDatabase ctx)
+    [Route("api/folha")]
+    [ApiController]
+    public class FolhaPagamentoController : ControllerBase
     {
-        _ctx = ctx;
+        private readonly ContextoDb _ctx; 
+
+        public FolhaPagamentoController(ContextoDb ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+        [HttpPost("criar")]
+        public async Task<IActionResult> CriarFuncionario([FromBody] FuncionarioDTO funcionarioDTO)
+        {
+            if (funcionarioDTO == null)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            var funcionario = new Funcionario
+            {
+                Nome = funcionarioDTO.Nome,
+            };
+
+            _ctx.Funcionarios.Add(funcionario);
+            await _ctx.SaveChangesAsync();
+
+            return Ok("Funcionário criado com sucesso");
+        }
     }
-
-
-    //Metodos
 }
--- ProvaVisual/Prova/Models/FolhaPagamento.cs
+++ ProvaVisual/Prova/Models/FolhaPagamento.cs
@@ -4,8 +4,8 @@ public class FolhaPagamento
  public int FolhaId { get; set; }
  public double ValorHora { get; set; }
  public int QuantidadeHoras { get; set; }
- public int Mes{ get; set; }
- public int Ano{ get; set; }
+ public int Mes { get; set; }
+ public int Ano { get; set; }
  public double SalarioBruto { get; set; }
  public double ImpostoRenda  { get; set; }
  public double INSS { get; set; }
