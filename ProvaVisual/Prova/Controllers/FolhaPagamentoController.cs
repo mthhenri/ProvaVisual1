8fff0b9dee8539ce4e52efc09500ec457285c708
aaaaa <aaaaa@aaaa.br>
2023-10-06 10:06:35 -0300

    CRUDs da lista

--- ProvaVisual/Prova/Controllers/FolhaPagamentoController.cs
+++ ProvaVisual/Prova/Controllers/FolhaPagamentoController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Prova.Controllers; 
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Prova.Controllers;
+using Prova.Data;
+using Prova.DTOs;
+using Prova.Models;
 
 namespace Prova.Controllers
 {
@@ -10,31 +10,67 @@ namespace Prova.Controllers
     [ApiController]
     public class FolhaPagamentoController : ControllerBase
     {
-        private readonly ContextoDb _ctx; 
+        private readonly AppDatabase _ctx; 
 
-        public FolhaPagamentoController(ContextoDb ctx)
+        public FolhaPagamentoController(AppDatabase ctx)
         {
             _ctx = ctx;
         }
 
 
         [HttpPost("criar")]
-        public async Task<IActionResult> CriarFuncionario([FromBody] FuncionarioDTO funcionarioDTO)
+        public async Task<IActionResult> CriarFolha([FromBody] FolhaPagamentoDTO folhaPagamentoDTO)
         {
-            if (funcionarioDTO == null)
-            {
-                return BadRequest("Dados inválidos");
-            }
+            if (folhaPagamentoDTO == null)
+        {
+            return BadRequest("Dados inválidos");
+        }
 
-            var funcionario = new Funcionario
-            {
-                Nome = funcionarioDTO.Nome,
-            };
+        var folhaPagamento = new FolhaPagamento
+        {
+            FuncionarioId = folhaPagamentoDTO.FuncionarioId,
+        };
 
-            _ctx.Funcionarios.Add(funcionario);
+            _ctx.Folhas.Add(folhaPagamento);
             await _ctx.SaveChangesAsync();
 
-            return Ok("Funcionário criado com sucesso");
+            return Ok("Folha de pagamento criada com sucesso!");
+        }
+
+        [HttpGet("listar")]
+        public IActionResult ListarFolhas()
+        {
+            var folhas = _ctx.Folhas.ToList();
+            return Ok(folhas);
+        }
+
+        [HttpGet("listarPorCpfMesAno")]
+        public IActionResult ListarFolhaPorCpfMesAno(string cpf, int mes, int ano)
+        {
+        var funcionario = _ctx.Funcionarios.FirstOrDefault(f => f.CPF == cpf);
+
+            if (funcionario == null)
+            {
+                return NotFound("Funcionário não encontrado");
+            }
+
+        var folhaPagamento = _ctx.Folhas.FirstOrDefault(fp => 
+            fp.FuncionarioId == funcionario.FuncionarioId &&
+            fp.Mes == mes &&
+            fp.Ano == ano);
+
+        if (folhaPagamento == null)
+            {
+                return NotFound("Folha de pagamento não encontrada");
+            }
+
+        var resultado = new
+            {
+                CPF = funcionario.CPF,
+                Mes = folhaPagamento.Mes,
+                Ano = folhaPagamento.Ano
+            };
+        return Ok(resultado);
         }
     }
 }
